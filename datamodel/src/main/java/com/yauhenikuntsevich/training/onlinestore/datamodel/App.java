2b68a1f267ace2c1a24c086ea328885b4088fd61
Pegas7 <ifk88@mail.ru>
2016-10-17 13:29:25 +0300

    project structure

---
+++ datamodel/src/main/java/com/yauhenikuntsevich/training/onlinestore/datamodel/App.java
@@ -0,0 +1,13 @@
+package com.yauhenikuntsevich.training.onlinestore.datamodel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
