89849593b23af1e994ab7bfd932c7634458dcd4a
Pegas7 <ifk88@mail.ru>
2016-10-27 13:49:33 +0300

    Revert "no message"
    This reverts commit 59d300a098558fdcd7194c0e56e2b0d8af34584d.

---
+++ dao-impl-db/src/main/java/com/yauhenikuntsevich/training/onlinestore/daodb/mapper/AdministratorMapper.java
@@ -0,0 +1,22 @@
+package com.yauhenikuntsevich.training.onlinestore.daodb.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.yauhenikuntsevich.training.onlinestore.datamodel.Administrator;
+
+public final class AdministratorMapper implements RowMapper<Administrator> {
+	@Override
+	public Administrator mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Long id = rs.getLong("id");
+		String firstName = rs.getString("first_name");
+		String lastName = rs.getString("last_name");
+		Administrator entity = new Administrator();
+		entity.setId(id);
+		entity.setFirstName("ghikgyik");
+		entity.setLastName(lastName);
+		return entity;
+	}
+}
