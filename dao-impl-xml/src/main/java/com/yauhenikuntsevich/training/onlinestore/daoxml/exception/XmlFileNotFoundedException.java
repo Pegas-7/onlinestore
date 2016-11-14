814747973a7700d387ea61e47ca1c66001973b96
Pegas7 <ifk88@mail.ru>
2016-11-14 22:08:35 +0300

    Added exception

---
+++ dao-impl-xml/src/main/java/com/yauhenikuntsevich/training/onlinestore/daoxml/exception/XmlFileNotFoundedException.java
@@ -0,0 +1,5 @@
+package com.yauhenikuntsevich.training.onlinestore.daoxml.exception;
+
+public class XmlFileNotFoundedException extends RuntimeException{
+	private static final long serialVersionUID = 1464367L;
+}
