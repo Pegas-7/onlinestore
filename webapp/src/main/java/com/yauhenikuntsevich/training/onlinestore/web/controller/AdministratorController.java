0726f72a1bfc87d36da55c3596076a16926e8904
Pegas7 <ifk88@mail.ru>
2016-11-25 11:12:36 +0300

    Added AdministratorController

--- webapp/src/main/java/com/yauhenikuntsevich/training/onlinestore/web/controller/AdministratorController.java
+++ webapp/src/main/java/com/yauhenikuntsevich/training/onlinestore/web/controller/AdministratorController.java
@@ -7,6 +7,8 @@ import javax.inject.Inject;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,34 +34,34 @@ public class AdministratorController {
 		return new ResponseEntity<List<AdministratorModel>>(converted, HttpStatus.OK);
 	}
 
-	/*
-	 * @RequestMapping(value = "/{authorId}", method = RequestMethod.GET) public
-	 * ResponseEntity<AdministratorModel> getById(@PathVariable Long authorId) {
-	 * Author author = service.get(authorId); return new
-	 * ResponseEntity<AdministratorModel>(entity2model(author), HttpStatus.OK);
-	 * }
-	 * 
-	 * @RequestMapping(method = RequestMethod.POST) public ResponseEntity<Void>
-	 * createNewAuthor(@RequestBody AdministratorModel authorModel) {
-	 * service.create(model2entity(authorModel)); return new
-	 * ResponseEntity<Void>(HttpStatus.CREATED);
-	 * 
-	 * }
-	 * 
-	 * @RequestMapping(value = "/{authorId}", method = RequestMethod.POST)
-	 * public ResponseEntity<Void> updateAuthor(@RequestBody AdministratorModel
-	 * authorModel, @PathVariable Long authorId) { Author author =
-	 * model2entity(authorModel); author.setId(authorId);
-	 * service.update(author); return new ResponseEntity<Void>(HttpStatus.OK);
-	 * 
-	 * }
-	 * 
-	 * @RequestMapping(value = "/{authorId}", method = RequestMethod.DELETE)
-	 * public ResponseEntity<Void> delete(@PathVariable Long authorId) {
-	 * service.delete(authorId); return new ResponseEntity<Void>(HttpStatus.OK);
-	 * 
-	 * }
-	 */
+	@RequestMapping(value = "/{administratorId}", method = RequestMethod.GET)
+	public ResponseEntity<AdministratorModel> getById(@PathVariable Long administratorId) {
+		Administrator author = administratorService.get(administratorId);
+		return new ResponseEntity<AdministratorModel>(administrator2model(author), HttpStatus.OK);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> createNewAuthor(@RequestBody AdministratorModel administratorModel) {
+		administratorService.save(model2administrator(administratorModel));
+		return new ResponseEntity<Void>(HttpStatus.CREATED);
+
+	}
+
+	@RequestMapping(value = "/{administratorId}", method = RequestMethod.POST)
+	public ResponseEntity<Void> updateAuthor(@RequestBody AdministratorModel administratorModel,
+			@PathVariable Long administratorId) {
+		Administrator author = model2administrator(administratorModel);
+		author.setId(administratorId);
+		administratorService.save(author);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+
+	}
+
+	@RequestMapping(value = "/{administratorId}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Long administratorId) {
+		administratorService.delete(administratorId);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
 
 	private AdministratorModel administrator2model(Administrator administrator) {
 		AdministratorModel e = new AdministratorModel();
@@ -76,5 +78,4 @@ public class AdministratorController {
 		e.setLastName(administratorModel.getLastName());
 		return e;
 	}
-
 }
